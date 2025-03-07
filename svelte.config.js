ab04fd32ce9f46b575001b26a0a312c85de56495
Atchyuteswar <yt.atchyut@gmail.com>
2025-03-07 08:32:22 +0530

    ...

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,11 @@ const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		adapter: adapter()
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter({
+			// any adapter-vercel specific options here
+			runtime: 'nodejs18.x'
+		})
 	}
 };
 
